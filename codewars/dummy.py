f8ab695be2ae11cbc6a202e9a3415ee89fb2f22d
Mike Weltevrede <46759318+mikeweltevrede@users.noreply.github.com>
2024-03-30 17:44:13 +0100

    Add dummy test

---
+++ codewars/dummy.py
@@ -0,0 +1,3 @@
+def dummy_func() -> int:
+    """Dummy func due to codecov requirements"""
+    return 42
---
+++ tests/unit/dummy_test.py
@@ -0,0 +1,5 @@
+from codewars import dummy
+
+
+def test_dummy_func():
+    assert dummy.dummy_func() == 42
