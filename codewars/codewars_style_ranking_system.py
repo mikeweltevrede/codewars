a282cf588b8cc187e4df5818a4a91c3989c618be
Mike Weltevrede <46759318+mikeweltevrede@users.noreply.github.com>
2024-03-31 14:45:51 +0200

    Codewars style ranking system (#2)

---
+++ codewars/codewars_style_ranking_system.py
@@ -0,0 +1,194 @@
+"""Codewars style ranking system
+
+Write a class called User that is used to calculate the amount that a user will progress through a ranking system
+similar to the one Codewars uses.
+
+Business Rules:
+- A user starts at rank -8 and can progress all the way to 8.
+- There is no 0 (zero) rank. The next rank after -1 is 1.
+- Users will complete activities. These activities also have ranks.
+- Each time the user completes a ranked activity the users rank progress is updated based off of the activity's rank
+- The progress earned from the completed activity is relative to what the user's current rank is compared to the rank of
+  the activity
+- A user's rank progress starts off at zero, each time the progress reaches 100 the user's rank is upgraded to the next
+  level
+- Any remaining progress earned while in the previous rank will be applied towards the next rank's progress (we don't
+  throw any progress away). The exception is if there is no other rank left to progress towards (Once you reach rank 8
+  there is no more progression).
+- A user cannot progress beyond rank 8.
+- The only acceptable range of rank values is -8,-7,-6,-5,-4,-3,-2,-1,1,2,3,4,5,6,7,8. Any other value should raise an
+  error.
+
+The progress is scored like so:
+- Completing an activity that is ranked the same as that of the user's will be worth 3 points
+- Completing an activity that is ranked one ranking lower than the user's will be worth 1 point
+- Any activities completed that are ranking 2 levels or more lower than the user's ranking will be ignored
+- Completing an activity ranked higher than the current user's rank will accelerate the rank progression. The greater
+  the difference between rankings the more the progression will be increased. The formula is 10 * d * d where d equals
+  the difference in ranking between the activity and the user.
+
+Logic Examples:
+- If a user ranked -8 completes an activity ranked -7 they will receive 10 progress
+- If a user ranked -8 completes an activity ranked -6 they will receive 40 progress
+- If a user ranked -8 completes an activity ranked -5 they will receive 90 progress
+- If a user ranked -8 completes an activity ranked -4 they will receive 160 progress, resulting in the user being
+  upgraded to rank -7 and having earned 60 progress towards their next rank
+- If a user ranked -1 completes an activity ranked 1 they will receive 10 progress (remember, zero rank is ignored)
+
+Code Usage Examples:
+    user = User()
+    user.rank # => -8
+    user.progress # => 0
+    user.inc_progress(-7)
+    user.progress # => 10
+    user.inc_progress(-5) # will add 90 progress
+    user.progress # => 0 # progress is now zero
+    user.rank # => -7 # rank was upgraded to -7
+
+Note: Codewars no longer uses this algorithm for its own ranking system. It uses a pure Math based solution that gives
+consistent results no matter what order a set of ranked activities are completed at.
+"""
+from typing import Optional
+
+
+class User:
+    min_rank = -8
+    max_rank = 8
+    max_progress = 100
+
+    def __init__(self) -> None:
+        """User in a ranking system similar to the one Codewars uses."""
+        self._rank = self.min_rank
+        self._progress = 0
+
+    @property
+    def rank(self) -> int:
+        """The user's rank in the system."""
+        return self._rank
+
+    @rank.setter
+    def rank(self, value: int) -> None:
+        self.validate_rank(rank=value)
+        self._rank = value
+
+    @property
+    def progress(self) -> int:
+        """The user's progress within their rank."""
+        return self._progress
+
+    @progress.setter
+    def progress(self, value: int) -> None:
+        self._progress = value
+
+    @property
+    def max_rank_increases(self) -> int:
+        """Number of ranks that the user can still increase. Note that we subtract 1 because 0 is not included."""
+        if self.rank < 0 < self.max_rank:
+            return self.max_rank - self.rank - 1
+        return self.max_rank - self.rank
+
+    @property
+    def valid_ranks(self) -> list[int]:
+        """Ranks that can be used for the user or activities."""
+        return [rank for rank in range(self.min_rank, self.max_rank + 1) if rank != 0]
+
+    def validate_rank(self, rank: int) -> None:
+        """Validate the value of `rank` versus the `valid_ranks` property.
+
+        :param rank: Rank to validate.
+        """
+        if rank not in self.valid_ranks:
+            raise ValueError(f"Rank must be in {self.valid_ranks} but was {rank}")
+
+    def inc_progress(self, rank_activity: int) -> None:
+        """Increase the progress based on the rank of the activity compared to the user's rank.
+
+        :param rank_activity: Rank of the completed activity.
+        """
+        print(f"Start: {self.rank=}, {self.progress=}, {rank_activity=}")
+        self.validate_rank(rank=rank_activity)
+
+        if self.rank == self.max_rank:
+            return
+
+        rank_difference = self._difference_to_user_rank(rank=rank_activity)
+
+        if rank_difference <= -2:
+            return
+
+        if rank_difference == -1:
+            self.progress += 1
+        elif rank_difference == 0:
+            self.progress += 3
+        else:  # rank_activity > self.rank - not sure if it is best practice to put else or the conditional
+            self.progress += self._progress_increase_with_rank_acceleration(rank_difference=rank_difference)
+
+        print(f"Mid: {self.rank=}, {self.progress=}, {rank_activity=}")
+        self.inc_rank()
+        print(f"End: {self.rank=}, {self.progress=}, {rank_activity=}")
+
+    def inc_rank(self) -> None:
+        """Increase the rank based on the collected progress."""
+        if self.progress < self.max_progress:
+            # Note that the if-statement about progress can be considered obsolete because the calculations below with
+            # modulus would lead to the same result. However, it is better to exit early.
+            return
+
+        if self.rank == self.max_rank:
+            self.progress = 0
+            return
+
+        rank_increases, remainder_progress = divmod(self.progress, self.max_progress)
+
+        if rank_increases >= self.max_rank_increases:
+            self.progress = 0
+            self.rank = self.max_rank
+            return
+
+        self.progress = remainder_progress
+
+        if self.rank < 0 <= self.rank + rank_increases:
+            self.rank += rank_increases + 1
+        else:
+            self.rank += rank_increases
+
+    def _difference_to_user_rank(self, rank: int) -> int:
+        """Computes the number of ranks between `rank` and the user's rank.
+
+        :param rank: Rank to find the difference for.
+        :return: Difference between `rank` and the user's rank.
+        """
+        difference = rank - self.rank
+
+        # 0 is not a valid rank. As such, if the sign changes, we have to subtract or add 1.
+        if self.rank < 0 < rank:
+            difference -= 1
+        elif self.rank > 0 > rank:
+            difference += 1
+        return difference
+
+    def _progress_increase_with_rank_acceleration(
+        self, rank_activity: Optional[int] = None, rank_difference: Optional[int] = None
+    ) -> int:
+        """Formula to compute rank increase for completion of tasks higher than the user's current rank.
+
+        Completing an activity ranked higher than the current user's rank will accelerate the rank progression. The
+        greater the difference between rankings the more the progression will be increased. The formula is 10 * d * d
+        where d equals the difference in ranking between the activity and the user.
+
+        :param rank_activity: Rank of the completed activity.
+        :param rank_difference: Difference between the rank of the completed activity and the user's rank.
+        :return: How much the progress increases.
+        """
+        if rank_activity is None and rank_difference is None:
+            raise ValueError("Either rank_activity or rank_difference should be provided, but none were provided.")
+
+        if rank_activity is not None:
+            if rank_difference is not None:
+                raise ValueError("Either rank_activity or rank_difference should be provided, but both were provided.")
+            rank_difference = self._difference_to_user_rank(rank=rank_activity)
+
+        if rank_difference < 0:
+            raise ValueError("Progress acceleration only possible for activities ranked higher than user's rank.")
+
+        return 10 * rank_difference**2
---
+++ tests/unit/codewars_style_ranking_system_test.py
@@ -0,0 +1,328 @@
+from unittest import mock
+
+import pytest
+
+from codewars.codewars_style_ranking_system import User
+
+
+class TestUser:
+    def test_user_starts_at_rank_neg8(self):
+        assert User().rank == -8
+
+    def test_user_starts_at_0_progress(self):
+        assert User().progress == 0
+
+    def test_rank_cannot_be_0(self):
+        with pytest.raises(ValueError, match="Rank must be in"):
+            User().rank = 0
+
+    def test_rank_cannot_be_lower_than_neg8(self):
+        with pytest.raises(ValueError, match="Rank must be in"):
+            User().rank = -9
+
+    def test_rank_cannot_be_higher_than_8(self):
+        with pytest.raises(ValueError, match="Rank must be in"):
+            User().rank = 9
+
+    def test_inc_progress_at_max_rank_does_not_change_progress(self):
+        user = User()
+        user.rank = user.max_rank
+
+        user.inc_progress(rank_activity=8)
+        assert user.progress == 0
+
+    def test_inc_progress_with_same_rank_as_user_adds_3_progress(self):
+        user = User()
+
+        user.inc_progress(rank_activity=-8)
+        assert user.progress == 3
+
+    def test_inc_progress_with_one_rank_lower_as_user_adds_1_progress(self):
+        user = User()
+        user.rank = -7
+
+        user.inc_progress(rank_activity=-8)
+        assert user.progress == 1
+
+    def test_inc_progress_with_one_rank_lower_as_user_adds_1_progress_for_rank_1(self):
+        user = User()
+        user.rank = 1
+
+        user.inc_progress(rank_activity=-1)
+        assert user.progress == 1
+
+    def test_inc_progress_with_more_than_one_rank_lower_as_user_adds_no_progress(self):
+        user = User()
+        user.rank = -6
+
+        user.inc_progress(rank_activity=-8)
+        assert user.progress == 0
+
+    def test_inc_progress_with_higher_rank_as_user_adds_accelerated_progress(self):
+        user = User()
+        user.rank = 2
+        user._progress_increase_with_rank_acceleration = mock.Mock(return_value=42)
+
+        user.inc_progress(rank_activity=4)
+        assert user.progress == 42
+
+    def test_inc_progress_calls_inc_rank(self):
+        user = User()
+        user._progress_increase_with_rank_acceleration = mock.Mock(return_value=42)
+        user.inc_rank = mock.Mock()
+
+        user.inc_progress(rank_activity=-5)
+        user.inc_rank.assert_called_once()
+
+    def test_inc_progress_raises_valueerror_for_invalid_rank(self):
+        user = User()
+        with pytest.raises(ValueError, match="Rank must be in"):
+            user.inc_progress(rank_activity=-9)
+
+    def test_inc_rank_increases_neg1_to_1(self):
+        user = User()
+        user.rank = -1
+        user.progress = user.max_progress
+
+        user.inc_rank()
+        assert user.rank == 1
+
+    def test_inc_rank_can_increases_neg5_to_5(self):
+        user = User()
+        user.rank = -5
+        user.progress = 9 * user.max_progress
+
+        user.inc_rank()
+        assert user.rank == 5
+
+    def test_inc_rank_when_progress_is_lower_than_max_progress_does_not_increase_rank(self):
+        user = User()
+        user.rank = 3
+        user.progress = 42
+
+        user.inc_rank()
+        assert user.rank == 3
+
+    def test_inc_rank_when_progress_is_equal_to_max_progress_increase_rank_by_1_if_rank_not_maxed(self):
+        user = User()
+        user.rank = 3
+        user.progress = 100
+
+        user.inc_rank()
+        assert user.rank == 4
+
+    def test_inc_rank_does_not_change_rank_if_rank_is_maxed(self):
+        user = User()
+        user.rank = 8
+        user.progress = 100
+
+        user.inc_rank()
+        assert user.rank == 8
+
+    def test_inc_rank_resets_progress_if_rank_is_maxed(self):
+        user = User()
+        user.rank = 8
+        user.progress = 100
+
+        user.inc_rank()
+        assert user.progress == 0
+
+    def test_inc_rank_when_progress_is_equal_to_max_progress_resets_progress_to_0(self):
+        user = User()
+        user.rank = 3
+        user.progress = 100
+
+        user.inc_rank()
+        assert user.progress == 0
+
+    def test_inc_rank_when_progress_is_equal_to_max_progress_plus_n_resets_progress_to_n(self):
+        user = User()
+        user.rank = 3
+        user.progress = 142
+
+        user.inc_rank()
+        assert user.progress == 42
+
+    def test_inc_rank_when_progress_exceeds_multiple_of_max_progress_raises_rank_by_multiple_levels_if_it_does_not_make_the_user_reach_max_rank(  # noqa: E501
+        self,
+    ):
+        user = User()
+        user.rank = 3
+        user.progress = 242
+
+        user.inc_rank()
+        assert user.rank == 5
+
+    def test_inc_rank_when_progress_exceeds_multiple_of_max_progress_sets_progress_to_remainder_if_it_does_not_make_the_user_reach_max_rank(  # noqa: E501
+        self,
+    ):
+        user = User()
+        user.rank = 3
+        user.progress = 242
+
+        user.inc_rank()
+        assert user.progress == 42
+
+    def test_inc_rank_when_progress_exceeds_multiple_of_max_progress_raises_rank_till_at_most_max_rank(self):
+        user = User()
+        user.rank = user.max_rank - 2
+        user.progress = 342
+
+        user.inc_rank()
+        assert user.rank == user.max_rank
+
+    def test_inc_rank_when_progress_exceeds_multiple_of_max_progress_which_raises_rank_till_max_rank_drops_remaining_progress_after_increase(  # noqa: E501
+        self,
+    ):
+        user = User()
+        user.rank = user.max_rank - 2
+        user.progress = 342
+
+        user.inc_rank()
+        assert user.progress == 0
+
+    @pytest.mark.parametrize(
+        ("rank_user", "rank_activity", "expected"),
+        [(5, 7, 2), (-7, -3, 4)],
+    )
+    def test__difference_to_user_rank_for_same_sign_returns_correct_value(
+        self, rank_user: int, rank_activity: int, expected: int
+    ):
+        user = User()
+        user.rank = rank_user
+
+        assert user._difference_to_user_rank(rank=rank_activity) == expected
+
+    @pytest.mark.parametrize(
+        ("rank_user", "rank_activity", "expected"),
+        [(-5, 7, 11), (-7, 3, 9), (-1, 1, 1)],
+    )
+    def test__difference_to_user_rank_for_differing_sign_returns_correct_value(
+        self, rank_user: int, rank_activity: int, expected: int
+    ):
+        user = User()
+        user.rank = rank_user
+
+        assert user._difference_to_user_rank(rank=rank_activity) == expected
+
+    @pytest.mark.parametrize(
+        ("rank_user", "rank_activity", "expected"),
+        [
+            (-8, -7, 10),
+            (-8, -6, 40),
+            (-8, -5, 90),
+            (-8, -4, 160),
+            (-1, 1, 10),
+            (-2, 2, 90),
+        ],
+    )
+    def test__progress_increase_with_rank_acceleration_returns_10_times_difference_squared(
+        self, rank_user: int, rank_activity: int, expected: int
+    ):
+        """Requirements as provided in the kata description.
+
+        - If a user ranked -8 completes an activity ranked -7 they will receive 10 progress
+        - If a user ranked -8 completes an activity ranked -6 they will receive 40 progress
+        - If a user ranked -8 completes an activity ranked -5 they will receive 90 progress
+        - If a user ranked -8 completes an activity ranked -4 they will receive 160 progress, resulting in the user
+          being upgraded to rank -7 and having earned 60 progress towards their next rank
+        - If a user ranked -1 completes an activity ranked 1 they will receive 10 progress (remember, zero rank is
+          ignored)
+        """
+        user = User()
+        user.rank = rank_user
+
+        actual = user._progress_increase_with_rank_acceleration(rank_activity=rank_activity)
+
+        assert actual == expected
+
+    def test__progress_increase_with_rank_acceleration_raises_value_error_if_rank_activity_and_rank_difference_are_provided(  # noqa: E501
+        self,
+    ):
+        user = User()
+
+        with pytest.raises(
+            ValueError, match="Either rank_activity or rank_difference should be provided, but both were provided."
+        ):
+            user._progress_increase_with_rank_acceleration(rank_activity=4, rank_difference=2)
+
+    def test__progress_increase_with_rank_acceleration_raises_value_error_if_neither_rank_activity_or_rank_difference_is_provided(  # noqa: E501
+        self,
+    ):
+        user = User()
+
+        with pytest.raises(
+            ValueError, match="Either rank_activity or rank_difference should be provided, but none were provided."
+        ):
+            user._progress_increase_with_rank_acceleration(rank_activity=None, rank_difference=None)
+
+    def test__progress_increase_with_rank_acceleration_raises_value_error_if_rank_of_user_is_above_rank_activity(self):
+        user = User()
+        user.rank = 5
+
+        with pytest.raises(
+            ValueError, match="Progress acceleration only possible for activities ranked higher than user's rank."
+        ):
+            user._progress_increase_with_rank_acceleration(rank_activity=4)
+
+
+class TestUseCasesCodewars:
+    @pytest.mark.parametrize(
+        ("rank_user", "rank_activity", "expected_rank", "expected_progress"),
+        [
+            (-8, -8, -8, 3),
+            (-8, -7, -8, 10),
+            (-8, -5, -8, 90),
+            (-8, -4, -7, 60),
+            (8, 8, 8, 0),
+            (-8, 4, 5, 10),
+        ],
+    )
+    def test_simple_use_cases_provided_by_code_wars(
+        self, rank_user: int, rank_activity: int, expected_rank: int, expected_progress: int
+    ):
+        user = User()
+        user.rank = rank_user
+        user.inc_progress(rank_activity=rank_activity)
+
+        assert user.rank == expected_rank
+        assert user.progress == expected_progress
+
+    @pytest.mark.parametrize(
+        ("rank_user", "progress_user", "rank_activity", "expected_rank", "expected_progress"),
+        [
+            (1, 20, -1, 1, 21),
+            (7, 91, 8, 8, 0),
+            (-3, 3, 8, 8, 0),
+            (6, 91, 8, 7, 31),
+        ],
+    )
+    def test_simple_use_cases_provided_by_code_wars_with_custom_progress(
+        self, rank_user: int, progress_user: int, rank_activity: int, expected_rank: int, expected_progress: int
+    ):
+        user = User()
+        user.rank = rank_user
+        user.progress = progress_user
+
+        user.inc_progress(rank_activity)
+        assert user.rank == expected_rank
+        assert user.progress == expected_progress
+
+    def test_multiple_calls_with_large_distance(self):
+        user = User()
+        user.inc_progress(rank_activity=1)
+        assert user.rank == -2
+        assert user.progress == 40
+
+        user.inc_progress(rank_activity=1)
+        assert user.rank == -2
+        assert user.progress == 80
+
+    def test_multiple_calls_at_max_rank(self):
+        user = User()
+        user.rank = 8
+        user.inc_progress(rank_activity=1)
+        assert user.rank == 8
+
+        user.inc_progress(rank_activity=2)
+        assert user.rank == 8
